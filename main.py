2427b459c38abc1d959f9d1ada8b70de0b60cb0f
Gamattowicz <p.romanczuk31@gmail.com>
2024-01-17 13:24:00 +0100

    Refactor load_movies_to_database endpoint and add fetch_all movie endpoint

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import json
+from typing import List
 
 from fastapi import FastAPI
 
 from database import database, movie_table
+from models import Movie
 
 app = FastAPI()
 
@@ -12,11 +14,25 @@ def home():
     return {"Hello": "World"}
 
 
-@app.get("/movies")
+@app.post("/movies", response_model=dict, status_code=201)
 async def load_movies_to_database():
     with open("movies_examples.json", "r") as f:
         data = json.load(f)
-    query = movie_table.insert().values(data)
     await database.connect()
-    await database.execute(query)
+    for movie in data:
+        query = movie_table.select().where(movie_table.c.id == movie["id"])
+        result = await database.fetch_one(query)
+        if result is None:
+            query = movie_table.insert().values(movie)
+            await database.execute(query)
     await database.disconnect()
+    return {"status": "Movies loaded successfully"}
+
+
+@app.get("/movie", response_model=List[Movie])
+async def get_all_movies():
+    query = movie_table.select()
+    await database.connect()
+    results = await database.fetch_all(query)
+    await database.disconnect()
+    return results
